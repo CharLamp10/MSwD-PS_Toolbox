c5ac41eac0cd451fe69d1c69ae32319a399888b5
CharLamp10 <xaralabos10@hotmail.gr>
2025-07-08 16:55:32 +0400

    Update

--- functions/vout_parcellation.m
+++
@@ -1,6 +0,0 @@
-function out = vout_parcellation(job)
-    out = cfg_dep;
-    out.sname = 'Parcellation output file';
-    out.src_output = substruct('.','timeseries_files');
-    out.tgt_spec = struct('filter','mat','strtype','e');
-end
